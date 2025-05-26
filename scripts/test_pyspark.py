7fbcc369cf8d29cadedbf41e2aa3793112c64963
abdonrb <abdondjesusramirez@gmail.com>
2025-05-26 13:19:25 +0200

    update

--- scripts/test_pyspark.py
+++ scripts/test_pyspark.py
@@ -5,7 +5,6 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-# Glue setup
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -13,20 +12,16 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Lee los datos desde el catálogo (crawler)
 datasource0 = glueContext.create_dynamic_frame.from_catalog(
     database = "student_data",
-    table_name = "student_clean",  # el nombre real que generó tu crawler
+    table_name = "student_clean",
     transformation_ctx = "datasource0"
 )
 
-# Transforma a DataFrame de Spark
 df = datasource0.toDF()
 
-# 💧 Limpieza de datos
 df = df.dropna()
 
-# 🎯 Clasifica a los estudiantes según su nota final
 from pyspark.sql.functions import when
 
 df = df.withColumn(
@@ -36,10 +31,8 @@ df = df.withColumn(
     .otherwise("Low")
 )
 
-# Convierte de nuevo a DynamicFrame para escribir
 final_df = DynamicFrame.fromDF(df, glueContext, "final_df")
 
-# Guarda en S3 como archivo limpio (Parquet recomendado)
 glueContext.write_dynamic_frame.from_options(
     frame = final_df,
     connection_type = "s3",
